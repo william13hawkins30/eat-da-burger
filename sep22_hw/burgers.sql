9254f02c022df83baa1785a87edae30fc9e3c131
william13hawkins30 <37884730+william13hawkins30@users.noreply.github.com>
2018-11-13 15:40:55 -0500

    Add files via upload

---
+++ sep22_hw/burgers.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers(
+	id INTEGER auto_increment not null,
+    burger_name VARCHAR(50),
+    devoured BOOLEAN DEFAULT false,
+    date TIMESTAMP not null DEFAULT current_timestamp,
+    primary key(id)
+    );
+    
+    
---
+++ sep22_hw/eat-da-burger/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured)
+	values('Hawaiian Burger', false),
+    ('Jalapeno Burger', false),
+    ('Kobi Burger', false);
+    
+    select * from burgers;
